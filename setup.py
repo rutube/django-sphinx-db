0eb5c61b80d24b6ead4ca71b9395573eb6b20554
Sergey Tikhonov <zimbler@gmail.com>
2013-02-20 13:07:09 +0400

    build increment: 0.1-4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 name = 'django_sphinx_db'
 version = '0.1'
-release = '3'
+release = '4'
 versrel = version + '-' + release
 readme = os.path.join(os.path.dirname(__file__), 'README.rst')
 download_url = 'https://github.com/downloads/smartfile/django-sphinx-db' \
