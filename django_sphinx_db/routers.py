14c425d22dbf0aa26d12adf4c559ca4da5f41193
Sergey Tikhonov <zimbler@gmail.com>
2013-12-11 10:46:58 +0400

    fix db_for_read for SphinxModel objects

--- django_sphinx_db/routers.py
+++ django_sphinx_db/routers.py
@@ -1,18 +1,27 @@
 from django.conf import settings
+from django.db.models.base import ModelBase
 
 
 class SphinxRouter(object):
     """
     Routes database operations for Sphinx model to the sphinx database connection.
     """
-    def db_for_read(self, model, **kwargs):
+
+    def is_sphinx_model(self, model_or_obj):
         from django_sphinx_db.backend.models import SphinxModel
-        if issubclass(model, SphinxModel):
+        if type(model_or_obj) is not ModelBase:
+            model = model_or_obj.__class__
+        else:
+            model = model_or_obj
+        is_sphinx_model = issubclass(model, SphinxModel)
+        return is_sphinx_model
+
+    def db_for_read(self, model, **kwargs):
+        if self.is_sphinx_model(model):
             return getattr(settings, 'SPHINX_DATABASE_NAME', 'sphinx')
 
     def db_for_write(self, model, **kwargs):
-        from django_sphinx_db.backend.models import SphinxModel
-        if issubclass(model, SphinxModel):
+        if self.is_sphinx_model(model):
             return getattr(settings, 'SPHINX_DATABASE_NAME', 'sphinx')
 
     def allow_relation(self, obj1, obj2, **kwargs):
